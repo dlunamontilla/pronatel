14eec4b18dd462d9fe290cc32aa5e143597a4a66
David E Luna M <davidlunamontilla@gmail.com>
2021-10-03 10:37:40 -0400

    Implementacion parcial de las vistas - parte 1

--- app/js/toggle.js
+++ app/js/toggle.js
@@ -1,8 +1,22 @@
 const toggles = document.querySelectorAll(".toggle-button");
 
-toggles.forEach((toggle) => {
-    toggle.onclick = function () {
-        this.classList.toggle("toggle-button--activo");
-    };
+const toggle = (element) => {
+    element.classList.toggle("toggle-button--activo");
+};
+
+toggles.forEach((toggleButton) => {
+    toggleButton.addEventListener("click", function () {
+        toggle(this);
+    });
+
+    toggleButton.addEventListener("keydown", function (e) {
+        const tecla = e.key;
+
+        if (tecla == "Enter" || tecla == " ") {
+            e.preventDefault();
+            toggle(this);
+        }
+    });
 });
+
 export {};
